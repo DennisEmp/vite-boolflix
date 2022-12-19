7bae7bac7a1182255c612930e504403219ce77f7
Dennis Empirio <dennis.empirio@gmail.com>
2022-12-19 09:44:58 +0100

    store aggiornato

--- src/store.js
+++ src/store.js
@@ -1,11 +1,6 @@
 import { reactive } from "vue"
 
 export const store = reactive ({
-    // apiURL: "https://api.themoviedb.org/3/movie/popular/?api_key=750d6010d296529b9a43762a8ba0f1c7",
-    // MovieList: [],
-
-    apiURL: "https://api.themoviedb.org/3/movie/popular/?api_key=a74482a62c6a03ab305fe588e15c57e3",
-    apiSearchMovie: "https://api.themoviedb.org/3/search/movie/?api_key=a74482a62c6a03ab305fe588e15c57e3",
+    apiURL: "https://api.themoviedb.org/3/movie/popular/?api_key=750d6010d296529b9a43762a8ba0f1c7",
     MovieList: [],
-    searchMovie: "",
 });
