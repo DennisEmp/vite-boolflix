1ab1507b01d0ee0691e35948ebfddbb59e6cd199
Dennis Empirio <dennis.empirio@gmail.com>
2022-12-20 13:23:20 +0100

    css work

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -20,10 +20,20 @@ export default {
             <label for="search-character" class="visually-hidden"> Search Character </label>
             <input type="text" class="form-controll" id="search-Film" placeholder="Search Film" v-model="store.searchMovie" />
         </div>
-        <div>
-            <button type="submit" class="btn btn-outline-danger mb-2" @click.prevent="$emit('searchTitle')"> Search </button>
-        </div>
+        <button class="cta" type="submit" @click.prevent="$emit('searchTitle')">
+            <span>Search</span>
+            <svg viewBox="0 0 13 10" height="10px" width="15px">
+                <path d="M1,5 L11,5"></path>
+                <polyline points="8 1 12 5 8 9"></polyline>
+            </svg>
+        </button>
+        <!-- <div>
+            <button type="submit" @click.prevent="$emit('searchTitle')"> Search </button>
+        </div> -->
     </div>
+
+
+    
 </template>
 
 
@@ -32,8 +42,61 @@ export default {
 .form-controll {
     padding-left: 10px;
 }
-.btn {
-    height: 28px;
-    margin-left: 10px;
+
+.cta {
+ position: relative;
+ margin: auto;
+ padding: 12px 18px;
+ transition: all 0.2s ease;
+ border: none;
+ background: none;
+}
+
+.cta:before {
+ content: "";
+ position: absolute;
+ top: 0;
+ left: 0;
+ display: block;
+ border-radius: 50px;
+ background: #b1dae7;
+ width: 45px;
+ height: 45px;
+ transition: all 0.3s ease;
+}
+
+.cta span {
+ position: relative;
+ font-family: "Ubuntu", sans-serif;
+ font-size: 18px;
+ font-weight: 700;
+ letter-spacing: 0.05em;
+ color: #234567;
+}
+
+.cta svg {
+ position: relative;
+ top: 0;
+ margin-left: 10px;
+ fill: none;
+ stroke-linecap: round;
+ stroke-linejoin: round;
+ stroke: #234567;
+ stroke-width: 2;
+ transform: translateX(-5px);
+ transition: all 0.3s ease;
+}
+
+.cta:hover:before {
+ width: 100%;
+ background: #ff2222;
+}
+
+.cta:hover svg {
+ transform: translateX(0);
+}
+
+.cta:active {
+ transform: scale(0.95);
 }
 </style>
